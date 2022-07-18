c435fa68bd23a8fcd188f1276e049489bd97c585
Armand XU <ziyi_xu@icloud.com>
2022-07-18 23:21:54 +0200

    等待测试

--- README.md
+++ README.md
@@ -25,3 +25,36 @@ company1,firstname1,lastname1,phone1,
 company2,firstname2,lastname2,phone1,phone2
 ...
 ```
+
+
+## Update on July 18 2022
+Recently Lusha updated their API with a bulk feature: [Lusha Person Bulk API](https://www.lusha.com/docs/#person-bulk-api)
+
+Instead of `GET https://api.lusha.com/person?firstName=:firstName&lastName=:lastName&company=:company&property=phoneNumbers`,
+`POST` request is used :
+```python
+POST https://api.lusha.com/person  -H api_key:API_KEY
+{
+ "filters":{
+   "property": (optional, one of ("emailAddresses", "phoneNumbers"))
+ },
+ "contacts":[
+  {
+   "contactId": (required, unique, type string)
+   "firstName": (required, type string),
+   "lastName": (required, type string),
+   "location": (optional, type object),
+   "linkedinUrl": (optional, type string), 
+   "companies": (required) [
+    {
+     "name": (required , type string, min_length=2),
+     "domain": (optional, type string, min_length=4),
+     "isCurrent":  (required, type bool),
+     "jobTitle": (optional, type string, min_length=2)
+     "fqdn": (optional, type string)
+    }
+   ]
+  }
+ ]
+}
+```
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import datetime
 import os
 import sys
 import csv
+import json
 
 import requests
 
@@ -14,18 +15,45 @@ API_KEY = os.getenv("API_KEY")
 # GLOBAL VARIABLES
 INPUT_LINE_COUNT = 0
 API_ERROR_COUNT = 0
+CONTACT_ID = 0
+RequestStruct = {
+    "filters": {
+        "property": "phoneNumbers"
+    }
+}
 
 
-class Person:
+class JSONHelper(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, PersonRequestStruct):
+            return o.__dict__
+        return super().default(o)
+
+
+class PersonRequestStruct:
     def __init__(self, company, first_name, last_name):
+        global CONTACT_ID
+        self.contactId = str(CONTACT_ID)
+        self.companies = [{
+            "name": company,
+            "isCurrent": True,
+        }]
+        self.firstName = first_name
+        self.lastName = last_name
+        CONTACT_ID += 1
+
+
+class PersonResStruct():
+    def __init__(self, contact_id, company, first_name, last_name, phone_number1, phone_number2):
+        self.contact_id = contact_id
         self.company = company
-        self.first_name = first_name
-        self.last_name = last_name
-        self.phone_number1 = ""
-        self.phone_number2 = ""
+        self.firstName = first_name
+        self.lastName = last_name
+        self.phone_number1 = phone_number1
+        self.phone_number2 = phone_number2
 
     def print_person(self):
-        print("\033[1;32m" + self.first_name + " " + self.last_name + "\033[0m" + " works in " +
+        print("\033[1;32m" + self.firstName + " " + self.lastName + "\033[0m" + " works in " +
               "\033[1;32m" + self.company + "\033[0m" + " phone number(s): " +
               "\033[1;32m" + self.phone_number1 + "\033[0m" + " and " + "\033[1;32m" + self.phone_number2 + "\033[0m")
 
@@ -39,39 +67,53 @@ def read_from_csv(file_name):
         reader = csv.reader(f)
         for row in reader:
             INPUT_LINE_COUNT += 1
-            persons.append(Person(row[0], row[1], row[2]))
+            persons.append(PersonRequestStruct(row[0], row[1], row[2]))
 
     return persons
 
 
-def write_to_csv(persons):
+def write_to_csv(persons: [PersonResStruct]):
     for person in persons:
         with open("output.csv", "a+") as f:
             f.write(
-                person.company + "," + person.first_name + "," + person.last_name + "," + person.phone_number1 + "," + person.phone_number2 + "\n")
+                person.contact_id + "," + person.company + "," + person.firstName + "," + person.lastName + "," +
+                person.phone_number1 + "," + person.phone_number2 + "\n")
 
 
 # To make routine work easier, we can use the Lusha API (not verified yet due to lack of the API key)
 # API Document : https://www.lusha.com/docs/#person-api
-def call_api(person: Person):
-    url = "https://api.lusha.com/person?firstName={0}&lastName={1}&company={2}&property=phoneNumbers" \
-        .format(person.first_name, person.last_name, person.company)
-    headers = {"api_key": API_KEY}
-    r = requests.get(url, headers=headers)
+def call_bulk_api(RequestStruct: dict):
+    url = "https://api.lusha.com/bulk/person"
+    headers = {'api_key': API_KEY, 'Content-type': 'application/json'}
+    payload = json.dumps(RequestStruct, cls=JSONHelper)
+    r = requests.post(url, headers=headers, data=payload)
     answer = r.json()
     return url, answer
 
 
-def api_response_handler(answer, person):
+def api_response_handler(answer, persons):
     global API_ERROR_COUNT
-    if "errors" in answer:
-        API_ERROR_COUNT += 1
-        print("\033[1;31mError: \033[0m" + str(answer))
-    else:
-        person.phone_number1 = answer["data"]["phoneNumbers"][0]["internationalNumber"]
-        if len(answer["data"]["phoneNumbers"]) > 1:
-            person.phone_number2 = answer["data"]["phoneNumbers"][1]["internationalNumber"]
-        person.print_person()
+    answer_contact = answer["contacts"]
+    persons_response = []
+
+    print("Got {0} answers from server.".format(len(answer_contact)))
+
+    for a in answer_contact.keys():
+        if "firstName" not in answer_contact[a]:
+            print("\033[1;31mError: \033[0m {0} - {1} : {2}"
+                  .format(str(persons[int(a)].contactId),
+                          str(persons[int(a)].firstName) + ' ' + str(persons[int(a)].lastName),
+                          str(answer_contact[a])))
+            API_ERROR_COUNT += 1
+        else:
+            contact = answer_contact[a]
+            phone_2 = contact["phones"][1] if len(contact["phones"]) == 2 else ""
+            person = PersonResStruct(a, persons[int(a)].companies[0]["name"], contact["firstName"], contact["lastName"],
+                                     contact["phones"][0], phone_2)
+            person.print_person()
+            persons_response.append(person)
+
+    return persons_response
 
 
 def save_request(request_url, request_answer):
@@ -87,12 +129,13 @@ def main():
     persons = read_from_csv(sys.argv[1])
     print("Successfully read {0} persons from {1}".format(INPUT_LINE_COUNT, sys.argv[1]))
 
-    for person in persons:
-        url, answer = call_api(person)
-        save_request(url, answer)
-        api_response_handler(answer, person)
+    RequestStruct["contacts"] = persons
 
-    write_to_csv(persons)
+    url, answer = call_bulk_api(RequestStruct)
+    save_request(url, answer)
+    persons_respons = api_response_handler(answer, persons)
+
+    write_to_csv(persons_respons)
 
     print("\033[1;32mFinished. Results can be found in output.csv\033[0m")
     print("\033[1;33mCouldn't find {0} of {1} persons in the list.\033[0m".format(API_ERROR_COUNT, INPUT_LINE_COUNT))
